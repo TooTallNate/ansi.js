d75b54de923ceb273eda4816776c83591db23348
Nathan Rajlich <nathan@tootallnate.net>
2012-03-10 14:03:28 -0800

    examples: add "clear" example
    like the ncurces "clear" command.

---
+++ examples/clear.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/**
+ * Like GNU ncurses "clear" command.
+ * https://github.com/mscdex/node-ncurses/blob/master/deps/ncurses/progs/clear.c
+ */
+
+require('../')(process.stdout)
+  .eraseData(2)
+  .goto(1, 1)
